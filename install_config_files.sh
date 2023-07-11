43f5707a22d68ce2a325cb97f0b6450ef4642571
Dustin Bragg <dustinlbragg@gmail.com>
2023-07-10 23:31:29 -0500

    Update install_config_files.sh

--- install_config_files.sh
+++ install_config_files.sh
@@ -43,7 +43,7 @@ sed -i -e "/user =/ s/= .*/= $Username/" /etc/lightdm/lightdm-mini-greeter.conf
 sudo systemctl enable lightdm.service -f
 
 #// Update font cache and permissions
-chmod -R 777 /usr/share/fonts/MyFonts
+chmod -R 755 /usr/share/fonts/MyFonts
 sudo -u $Username fc-cache -f
 
 #// Needed for backlight control
