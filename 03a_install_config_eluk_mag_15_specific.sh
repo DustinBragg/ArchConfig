b3d92232956f30ef354145ae9b5595e00de3967a
Dustin Bragg <dustinlbragg@gmail.com>
2024-10-20 17:34:27 -0500

    Fixed 03a_install_config_eluk_mag_15_specific.sh

--- 03a_install_config_eluk_mag_15_specific.sh
+++ 03a_install_config_eluk_mag_15_specific.sh
@@ -20,19 +20,19 @@ fi
 
 #// Install apps specific to this laptop config
 section_header_with_output "Installing packages..."
-yay -S --noconfirm --sudoloop --needed - < eluk_mag_15_additional_package_list.txt
+yay -S --noconfirm --sudoloop --needed - < config_package_list_eluk_mag_15_specific.txt
 
 #// Append new i3 config stuff (volume/brightness binds, among others)
 #// Copy the first config over to make this idempotent
 section_header "Copying over config/dot files..."
 rm $HOME/.config/i3/config
-cp -p $HOME/ArchConfig/files/home/.config/i3/config $HOME/.config/i3/config
+cp -p $HOME/ArchConfig/files/x_specific/home/.config/i3/config $HOME/.config/i3/config
 
 cat ./files/eluk_mag_15_specific/home/.config/i3/config_append >> $HOME/.config/i3/config
 
 #// Copy files over
-cp -r ./files_eluk_mag_15_additional/home/.config/volumeicon $HOME/.config/
-cp -p ./files_eluk_mag_15_additional/home/.config/i3/brightness_ctl.sh $HOME/.config/i3/
+cp -r ./files/eluk_mag_15_specific/home/.config/volumeicon $HOME/.config/
+cp -p ./files/eluk_mag_15_specific/home/.config/i3/brightness_ctl.sh $HOME/.config/i3/
 
 #// Output at the end
 echo ""
