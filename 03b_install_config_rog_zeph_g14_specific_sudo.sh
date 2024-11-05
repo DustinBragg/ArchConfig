45ebc8c67ec2c7f29bcc99f335238cf3073c753a
Dustin Bragg <dustinlbragg@gmail.com>
2024-11-04 22:52:50 -0600

    Update 03b_install_config_rog_zeph_g14_specific_sudo.sh

--- 03b_install_config_rog_zeph_g14_specific_sudo.sh
+++ 03b_install_config_rog_zeph_g14_specific_sudo.sh
@@ -56,6 +56,8 @@ sed -i 's/\"vfio_enable\": false/\"vfio_enable\": true/g' /etc/supergfxd.conf
 systemctl enable --now dgpu_toggle_forceoff.service
 
 #// Change GRUB menu text size
+sed -i 's/#GRUB_GFXMODE/GRUB_GFXMODE/g' /etc/default/grub
+sed -i 's/GRUB_GFXMODE=auto/GRUB_GFXMODE=1280x720/g' /etc/default/grub
 sed -i 's/gfxmode=auto/gfxmode=1280x720/g' /boot/grub/grub.cfg
 
 #// Output at the end
