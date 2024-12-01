70b1a8a97b0782e0b1185ccc64b74eb59932d0b1
Dustin Bragg <dustinlbragg@gmail.com>
2024-12-01 12:07:57 -0600

    Update i3blocks_CheckForUpdates.sh

--- files/x_specific/home/.config/i3/i3blocks_CheckForUpdates.sh
+++ files/x_specific/home/.config/i3/i3blocks_CheckForUpdates.sh
@@ -3,23 +3,21 @@
 #// Query pending updates and capture it as a number
 NumUpdates=$(checkupdates | wc -l)
 
-Color="#feca88"
-#Color="#aac474"
-#Color="#c55555"
+Color="#aac474"
 PluralOrNot="updates"
 if [ $NumUpdates -eq 1 ]; then
 PluralOrNot="updates"
 fi
 
-if [ $NumUpdates -gt 8 ]; then
-Color="#aac474"
+if [ $NumUpdates -gt 10 ]; then
+Color="#feca88"
 fi
-if [ $NumUpdates -gt 15 ]; then
+if [ $NumUpdates -gt 20 ]; then
 Color="#c55555"
 fi
 
 if [ $NumUpdates -ge 0 ]; then
-echo " ${NumUpdates} ${PluralOrNot} available"
+echo " ${NumUpdates} ${PluralOrNot} available "
 echo 
 echo "${Color}"
 else
