c01f184acf9bdf2eb02c6162262f7d75de6c244f
Dustin Bragg <dustinlbragg@gmail.com>
2024-12-01 12:15:56 -0600

    Fixed typos

--- files/x_specific/home/.config/i3/i3blocks_CheckForUpdates.sh
+++ files/x_specific/home/.config/i3/i3blocks_CheckForUpdates.sh
@@ -6,7 +6,7 @@ NumUpdates=$(checkupdates | wc -l)
 Color="#aac474"
 PluralOrNot="updates"
 if [ $NumUpdates -eq 1 ]; then
-PluralOrNot="updates"
+PluralOrNot="update"
 fi
 
 if [ $NumUpdates -gt 10 ]; then
@@ -16,7 +16,7 @@ if [ $NumUpdates -gt 20 ]; then
 Color="#c55555"
 fi
 
-if [ $NumUpdates -ge 0 ]; then
+if [ $NumUpdates -gt 0 ]; then
 echo " ${NumUpdates} ${PluralOrNot} available "
 echo 
 echo "${Color}"
