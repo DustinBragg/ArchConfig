558d91fee5a47008da3b9876d37eef19f41948c7
Dustin Bragg <dustinlbragg@anadodic.com>
2025-12-14 21:04:47 -0600

    Increased range of update colors.

--- files/x_specific/home/.config/i3/i3blocks_CheckForUpdates.sh
+++ files/x_specific/home/.config/i3/i3blocks_CheckForUpdates.sh
@@ -3,23 +3,24 @@
 #// Query pending updates and capture it as a number
 NumUpdates=$(checkupdates | wc -l)
 
-Color="#aac474"
-PluralOrNot="updates"
-if [ $NumUpdates -eq 1 ]; then
-PluralOrNot="update"
-fi
-
-if [ $NumUpdates -gt 10 ]; then
-Color="#feca88"
-fi
-if [ $NumUpdates -gt 20 ]; then
-Color="#c55555"
-fi
-
 if [ $NumUpdates -gt 0 ]; then
-echo " ${NumUpdates} ${PluralOrNot} available "
-echo 
-echo "${Color}"
+    Color="#aac474"
+    PluralOrNot="updates"
+    if [ $NumUpdates -eq 1 ]; then
+	PluralOrNot="update"
+    fi
+
+    if [ $NumUpdates -gt 30 ]; then
+	Color="#feca88"
+    fi
+
+    if [ $NumUpdates -gt 60 ]; then
+	Color="#c55555"
+    fi
+
+    echo " ${NumUpdates} ${PluralOrNot} available "
+    echo 
+    echo "${Color}"
 else
-echo 
+    echo 
 fi
